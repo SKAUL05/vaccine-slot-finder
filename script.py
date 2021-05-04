853efb5218563bc69b26b78fa01a865cea2ff675
Sarath Kaul <kaul.sarath@gmail.com>
2021-05-04 05:59:24 +0000

    Mail Changes

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import json
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 from datetime import datetime, timedelta
@@ -22,17 +23,19 @@ for d in dates:
 
     for acent in jret:
         app_json = {"Name": "", "PinCode": "", "Capacity": [], "Date": []}
-        for sess in acent["sessions"]:
-            if sess["available_capacity"] > 0 and sess["min_age_limit"] == 45:
-                # print(sess)
-                app_json["Name"] = acent["name"]
-                app_json["PinCode"] = acent["pincode"]
-                app_json["Capacity"].append(sess["available_capacity"])
-                app_json["Date"].append(sess["date"])
+        try:
+            for sess in acent["sessions"]:
+                if sess["available_capacity"] > 0 and sess["min_age_limit"] == 45:
+                    # print(sess)
+                    app_json["Name"] = acent["name"]
+                    app_json["PinCode"] = acent["pincode"]
+                    app_json["Capacity"].append(sess["available_capacity"])
+                    app_json["Date"].append(sess["date"])
 
-        if app_json["Name"]:
-            pr_json.append(app_json)
-
+            if app_json["Name"]:
+                pr_json.append(app_json)
+        except Exception as e:
+            print(e)
 print(pr_json)
 
 if pr_json:
@@ -50,7 +53,7 @@ if pr_json:
             ],
             "from": {"email": "kaul.sarath@gmail.com", "name": "Vaccine Bot"},
             "subject": "Vaccine Slot Available",
-            "content": [{"type": "text/html", "value": pr_json}],
+            "content": [{"type": "text/html", "value": json.dumps(pr_json)}],
         }
         response = sg.client.mail.send.post(request_body=data)
 
