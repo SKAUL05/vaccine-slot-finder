c7db36b499f7ad608ece2a1466928d1ffecbfa7b
Sarath Kaul <kaul.sarath@gmail.com>
2021-05-04 05:49:19 +0000

    Mail Added

--- script.py
+++ script.py
@@ -1,8 +1,11 @@
 import requests
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from datetime import datetime, timedelta
+
 
 URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=230&date="
-
-from datetime import datetime, timedelta
 
 st_date = datetime.now() + timedelta(days=-(datetime.today().weekday() - 0))
 st_date_str = datetime.now().strftime("%d-%m-%Y")
@@ -15,13 +18,14 @@ pr_json = []
 for d in dates:
     URL_HIT = URL + d
     x = requests.get(URL_HIT)
-    # print(list(x.json().values())[0])
     jret = list(x.json().values())[0]
 
     for acent in jret:
+        if type(accent) != dict():
+            continue
         app_json = {"Name": "", "PinCode": "", "Capacity": [], "Date": []}
         for sess in acent["sessions"]:
-            if sess["available_capacity"] > 0 and sess["min_age_limit"] == 18:
+            if sess["available_capacity"] > 0 and sess["min_age_limit"] == 45:
                 # print(sess)
                 app_json["Name"] = acent["name"]
                 app_json["PinCode"] = acent["pincode"]
@@ -32,3 +36,28 @@ for d in dates:
             pr_json.append(app_json)
 
 print(pr_json)
+
+if pr_json:
+    try:
+        sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
+        data = {
+            "personalizations": [
+                {
+                    "to": [
+                        {
+                            "email": "kaul.sarath@gmail.com",
+                        }
+                    ]
+                }
+            ],
+            "from": {"email": "kaul.sarath@gmail.com", "name": "Vaccine Bot"},
+            "subject": "Vaccine Slot Available",
+            "content": [{"type": "text/html", "value": pr_json}],
+        }
+        response = sg.client.mail.send.post(request_body=data)
+
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e)
