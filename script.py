97ebacf631dd2e3aa076ad14f2314eb5dabbe5cf
Sarath Kaul <kaul.sarath@gmail.com>
2021-05-11 14:27:44 +0530

    Update script.py

--- script.py
+++ script.py
@@ -17,16 +17,16 @@ en_date_str = en_date.strftime("%d-%m-%Y")
 dates = [st_date_str, en_date_str]
 res_json = []
 request_header = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:88.0) Gecko/20100101 "
-    "Firefox/88.0",
-    "Accept": "application/json, text/plain, */*",
-    "Accept-Language": "en-US,en;q=0.5 --compressed",
-    "Origin": "https://selfregistration.cowin.gov.in",
-    "Connection": "keep-alive",
-    "Referer": "https://selfregistration.cowin.gov.in/",
-    "Pragma": "no-cache",
-    "Cache-Control": "no-cache",
-    "TE": "Trailers",
+    'authority': 'scrapeme.live',
+    'dnt': '1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'sec-fetch-site': 'none',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-user': '?1',
+    'sec-fetch-dest': 'document',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
 }
 proxy_list =  os.environ.get("PROXY").strip('][').split(',')
 proxies = {"https": proxy_list[random.randint(0, 2)]}
